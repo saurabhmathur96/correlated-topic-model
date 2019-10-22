68674be71f9789935cab9a54950087cd7ede0d21
Saurabh Mathur <saurabhmathur96@gmail.com>
2019-10-21 22:11:01 -0400

    Create lda.py

---
+++ lda.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def model(documents, word_count, topic_count):
+  ''' initializes the model parameters: theta (doc-topic), phi(topic-word) '''
+  pass
+
+def mcmc_inference(documents, model, alpha, beta, iteration_count):
+  ''' performs inference on lda model by gibbs sampling '''
+  pass
